3bc9969f66e52d6c9458e35fe715ca6d2b0f6d14
Natelle Ziegler <n.ziegler@ymail.com>
2018-01-13 00:58:31 -0500

    Solved problems up through book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,22 @@
 class Book
 # write your code here
+	def title=(name)
+		array = name.split(" ")
+		str = ""
+		array.each do |word|
+			if word != "and" && word != "the" && word != "over" && word != "in" && word != "of" && word != "a" && word != "an"
+				str += word[0].upcase + word[1, word.length]
+			elsif word == "the" && str == ""
+				str += word[0].upcase + word[1, word.length]
+			else
+				str += word
+			end
+			str += " "
+		end
+		@title = str.rstrip
+	end
+	
+	def title
+		@title
+	end
 end
